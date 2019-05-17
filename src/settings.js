13852878ec849ce48e3cb820555b1b3336ea0511
Cal Smith <calsmith@protonmail.com>
2019-05-16 23:16:06 -0400

    chore: add settings.js

---
+++ src/settings.js
@@ -0,0 +1,11 @@
+// borrowed from carbon-angular. tabbable === focusable
+const tabbableSelector = `
+  a[href], area[href], input:not([disabled]):not([tabindex='-1']),
+  button:not([disabled]):not([tabindex='-1']),select:not([disabled]):not([tabindex='-1']),
+  textarea:not([disabled]):not([tabindex='-1']),
+  iframe, object, embed, *[tabindex]:not([tabindex='-1']), *[contenteditable=true]
+`;
+
+// because we're going to have a bunch of exports
+// eslint-disable-next-line import/prefer-default-export
+export { tabbableSelector };
