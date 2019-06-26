6fc5f575e3a8d1cf4559e9512e0f30613699b25b
Akira Sudoh <asudoh@gmail.com>
2019-06-27 07:06:16 +0900

    chore(devenv): introduce prefix for devenv (#31)
    This change introduces a new prefix for custom element name as well as
    for BEM, `bx-ce-demo`, used only for our dev env. Doing so allows us to
    distinguish prefix for dev env from one for BEM prefix used only for
    `carbon-custom-elements`.

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -9,11 +9,11 @@ addDecorator(
     <style>
       ${containerStyles}
     </style>
-    <bx-ce-focus-trap href="#main-content" aria-label="Skip to main content">Skip to main content</bx-ce-focus-trap>
-    <div name="main-content" data-floating-menu-container role="main" class="bx--body bx-ce-devenv--container">
+    <bx-ce-demo-focus-trap href="#main-content" aria-label="Skip to main content">Skip to main content</bx-ce-demo-focus-trap>
+    <div name="main-content" data-floating-menu-container role="main" class="bx--body bx-ce-demo-devenv--container">
       ${story()}
     </div>
-    <bx-ce-focus-trap href="#main-content" aria-label="End of content">End of content</bx-ce-focus-trap>
+    <bx-ce-demo-focus-trap href="#main-content" aria-label="End of content">End of content</bx-ce-demo-focus-trap>
   `
 );
 
