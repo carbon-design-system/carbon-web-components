3b0beb2661b7df3bbad92eb067a24a22a6a44964
Akira Sudoh <asudoh@gmail.com>
2019-07-08 17:01:06 +0900

    chore(devenv): introdue theme switcher
    This change introduces a theme switcher in our Storybook, consisting
    three parts:
    
    * The theme switcher UI, implemented as a Storybook add-on
    * Sass code change to define CSS custom properties and to add a theme
      option to use CSS custom properties
    * Build change to combine CSS code with CSS custom properties and one
      without it. The latter works as an escape hatch for IE
    
    The theme switcher is roped off and requires an environment variable
    (`CARBON_CUSTOM_ELEMENTS_STORYBOOK_USE_CUSTOM_PROPERTIES`) to turn it
    on, given it incurs extra build time.

---
+++ .storybook/addon-carbon-theme/shared.js
@@ -0,0 +1,3 @@
+export const ADDON_ID = 'carbon-theme';
+export const CURRENT_THEME = `${ADDON_ID}/current`;
+export const PANEL_ID = `${ADDON_ID}/panel`;
--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,7 +1,9 @@
 import '../src/polyfills';
 import { html } from 'lit-html'; // eslint-disable-line import/first
+import addons from '@storybook/addons';
 import { configure, addDecorator, addParameters } from '@storybook/polymer'; // eslint-disable-line import/first
 import './components/focus-trap/focus-trap';
+import { CURRENT_THEME } from './addon-carbon-theme/shared';
 import theme from './theme';
 import containerStyles from './_container.scss'; // eslint-disable-line import/first
 
@@ -23,6 +25,10 @@ addDecorator(
     <bx-ce-demo-focus-trap href="#main-content" aria-label="End of content">End of content</bx-ce-demo-focus-trap>
   `
 );
+
+addons.getChannel().on(CURRENT_THEME, theme => {
+  document.documentElement.setAttribute('storybook-carbon-theme', theme);
+});
 
 function loadStories() {
   const req = require.context('../src/components', true, /\-story\.[jt]s$/);
