3009250c7f597c627e523a393a6849fec1afcd98
Akira Sudoh <asudoh@gmail.com>
2019-08-02 13:03:42 +0900

    feat(notification): introduce notification

---
+++ src/components/notification/notification-story.ts
@@ -0,0 +1,97 @@
+import { ifDefined } from 'lit-html/directives/if-defined';
+import { html } from 'lit-element';
+import { storiesOf } from '@storybook/polymer';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, select, text } from '@storybook/addon-knobs';
+import { NOTIFICATION_KIND } from './inline-notification';
+import './toast-notification';
+
+const kinds = {
+  [`Success (${NOTIFICATION_KIND.SUCCESS})`]: NOTIFICATION_KIND.SUCCESS,
+  [`Info (${NOTIFICATION_KIND.INFO})`]: NOTIFICATION_KIND.INFO,
+  [`Warning (${NOTIFICATION_KIND.WARNING})`]: NOTIFICATION_KIND.WARNING,
+  [`Error (${NOTIFICATION_KIND.ERROR})`]: NOTIFICATION_KIND.ERROR,
+};
+
+const createInlineProps = () => ({
+  kind: select('The notification kind (kind)', kinds, NOTIFICATION_KIND.INFO),
+  title: text('Title (title)', 'Notification title'),
+  subtitle: text('Subtitle (subtitle)', 'Subtitle text goes here.'),
+  hideCloseButton: boolean('Hide the close button (hide-close-button)', false),
+  closeButtonLabel: text('a11y label for the close button (close-button-label)', ''),
+  iconLabel: text('a11y label for the icon (icon-label)', ''),
+  open: boolean('Open (open)', true),
+  disableClose: boolean(
+    'Disable user-initiated close action (Call event.preventDefault() in bx-notification-beingclosed event)',
+    false
+  ),
+});
+
+const createToastProps = () => ({
+  ...createInlineProps(),
+  caption: text('Caption (caption)', 'Time stamp [00:00:00]'),
+});
+
+storiesOf('Notifications', module)
+  .addDecorator(withKnobs)
+  .add('Inline', () => {
+    const { kind, title, subtitle, hideCloseButton, closeButtonLabel, iconLabel, open, disableClose } = createInlineProps();
+    const beforeSelectedAction = action('bx-notification-beingclosed');
+    const handleBeforeClose = (event: CustomEvent) => {
+      beforeSelectedAction(event);
+      if (disableClose) {
+        event.preventDefault();
+      }
+    };
+    return html`
+      <bx-inline-notification
+        style="min-width: 30rem; margin-bottom: .5rem"
+        kind="${kind}"
+        title="${title}"
+        subtitle="${subtitle}"
+        ?hide-close-button="${hideCloseButton}"
+        close-button-label="${ifDefined(!closeButtonLabel ? undefined : closeButtonLabel)}"
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        ?open="${open}"
+        @bx-notification-beingclosed="${handleBeforeClose}"
+        @bx-notification-closed="${action('bx-notification-closed')}"
+      >
+      </bx-inline-notification>
+    `;
+  })
+  .add('Toast', () => {
+    const {
+      kind,
+      title,
+      subtitle,
+      caption,
+      hideCloseButton,
+      closeButtonLabel,
+      iconLabel,
+      open,
+      disableClose,
+    } = createToastProps();
+    const beforeSelectedAction = action('bx-notification-beingclosed');
+    const handleBeforeClose = (event: CustomEvent) => {
+      beforeSelectedAction(event);
+      if (disableClose) {
+        event.preventDefault();
+      }
+    };
+    return html`
+      <bx-toast-notification
+        style="min-width: 30rem; margin-bottom: .5rem"
+        kind="${kind}"
+        title="${title}"
+        subtitle="${subtitle}"
+        caption="${caption}"
+        ?hide-close-button="${hideCloseButton}"
+        close-button-label="${ifDefined(!closeButtonLabel ? undefined : closeButtonLabel)}"
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        ?open="${open}"
+        @bx-notification-beingclosed="${handleBeforeClose}"
+        @bx-notification-closed="${action('bx-notification-closed')}"
+      >
+      </bx-toast-notification>
+    `;
+  });
