458905488a1b04d1ceba252140a09247668b8402
Akira Sudoh <asudoh@gmail.com>
2019-08-09 16:03:29 +0900

    chore(build): optimize for modern browsers
    This change mainly does the following change:
    
    - Re-introduce the bundled build
    - Make the ESM build target modern browsers; Applications that still
      support IE11 should re-transpile to use the ESM build
    - Fix the entry-point to include recently-introduced components
    
    Also this change removes `babel-plugin-supercall`, which was used for
    extending native `HTMLElement` in ES5 code. As of today, no component
    in this library extends native `HTMLElement` directly.
    
    Fixes #74.
    Fixes #75.

--- gulp-tasks/build.js
+++ gulp-tasks/build.js
@@ -3,24 +3,24 @@
 const path = require('path');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
-const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const postcss = require('gulp-postcss');
 const prettier = require('gulp-prettier');
-const typescript = require('gulp-typescript');
 const through2 = require('through2');
 const autoprefixer = require('autoprefixer');
 const replaceExtension = require('replace-ext');
-const rollup = require('rollup');
+const { rollup } = require('rollup');
 const rollupConfigDev = require('../rollup.config.dev');
 const rollupConfigProd = require('../rollup.config.prod');
-const createSVGResultFromCarbonIcon = require('../tools/svg-result-carbon-icon');
 const babelPluginResourceJSPaths = require('../babel-plugin-resource-js-paths');
+const createSVGResultFromCarbonIcon = require('../tools/svg-result-carbon-icon');
 
 const config = require('./config');
+
+const buildProd = process.env.NODE_ENV === config.ENV_PRODUCTION;
 
 module.exports = {
   modules: {
@@ -51,13 +51,14 @@ module.exports = {
         .pipe(
           sass({
             includePaths: ['node_modules'],
-            outputStyle: process.env.NODE_ENV !== config.ENV_PRODUCTION ? 'nested' : 'compressed',
+            outputStyle: !buildProd ? 'nested' : 'compressed',
           }).on('error', sass.logError)
         )
         .pipe(
           postcss([
             autoprefixer({
-              browsers: ['> 1%', 'last 2 versions'],
+              // TODO: Optimize for modern browsers here
+              browsers: ['last 1 version', 'Firefox ESR', 'ie >= 11'],
             }),
           ])
         )
@@ -76,14 +77,15 @@ module.exports = {
     },
 
     scripts() {
-      const tsProject = typescript.createProject(path.resolve(__dirname, '../tsconfig.json'), {
-        isolatedModules: true,
-      });
       return gulp
-        .src([`${config.srcDir}/**/*.ts`, `!${config.srcDir}/**/*.d.ts`, `!${config.srcDir}/index-with-polyfills.ts`])
+        .src([
+          `${config.srcDir}/**/*.ts`,
+          `!${config.srcDir}/**/*-story*.ts*`,
+          `!${config.srcDir}/**/*.d.ts`,
+          `!${config.srcDir}/index-with-polyfills.ts`,
+        ])
         .pipe(plumber())
-        .pipe(gulpif(process.env.NODE_ENV !== config.ENV_PRODUCTION, sourcemaps.init()))
-        .pipe(tsProject())
+        .pipe(gulpif(!buildProd, sourcemaps.init()))
         .pipe(
           babel({
             presets: [
@@ -91,60 +93,38 @@ module.exports = {
                 '@babel/preset-env',
                 {
                   modules: false,
-                  targets: {
-                    browsers: ['last 1 version', 'Firefox ESR', 'not edge > 0', 'not ie > 0'],
-                  },
-                  exclude: ['transform-classes'],
+                  targets: ['last 1 version', 'Firefox ESR', 'not opera > 0', 'not android > 0', 'not edge > 0', 'not ie > 0'],
                 },
               ],
             ],
-            plugins: [babelPluginResourceJSPaths],
+            // `version: '7.3.0'` ensures `@babel/plugin-transform-runtime` is applied to decorator helper
+            plugins: [['@babel/plugin-transform-runtime', { version: '7.3.0' }], babelPluginResourceJSPaths],
           })
         )
-        .pipe(gulpif(process.env.NODE_ENV !== config.ENV_PRODUCTION, sourcemaps.write()))
+        .pipe(gulpif(!buildProd, sourcemaps.write()))
         .pipe(gulp.dest(config.jsDestDir));
     },
   },
 
-  sass() {
-    return gulp
-      .src(`${config.srcDir}/**/*.scss`)
-      .pipe(gulpif(process.env.NODE_ENV !== config.ENV_PRODUCTION, sourcemaps.init()))
-      .pipe(
-        sass({
-          includePaths: ['node_modules'],
-          outputStyle: process.env.NODE_ENV !== config.ENV_PRODUCTION ? 'nested' : 'compressed',
-        }).on('error', sass.logError)
-      )
-      .pipe(
-        postcss([
-          autoprefixer({
-            browsers: ['> 1%', 'last 2 versions'],
-          }),
-        ])
-      )
-      .pipe(concat('carbon-custom-elements.css'))
-      .pipe(gulpif(process.env.NODE_ENV !== config.ENV_PRODUCTION, sourcemaps.write()))
-      .pipe(gulp.dest(config.destDir));
-  },
-
-  scripts() {
+  bundle() {
+    const suffix = !buildProd ? '.js' : '.min.js';
     return Promise.all(
       Object.keys(config.bundle).map(moduleName =>
-        rollup
-          .rollup(
-            Object.assign(process.env.NODE_ENV === config.ENV_PRODUCTION ? rollupConfigProd : rollupConfigDev, {
-              entry: path.join(config.srcDir, config.bundle[moduleName]),
-            })
-          )
-          .then(bundle =>
-            bundle.write({
-              format: 'iife',
-              moduleName,
-              dest: path.join(config.destDir, `${moduleName}.js`),
-              sourceMap: true,
-            })
-          )
+        rollup(
+          Object.assign(buildProd ? rollupConfigProd : rollupConfigDev, {
+            input: path.join(config.srcDir, config.bundle[moduleName]),
+          })
+        ).then(bundle =>
+          bundle.write({
+            format: 'iife',
+            moduleName,
+            output: {
+              name: 'CarbonCustomElements',
+              file: path.join(config.destDir, moduleName + suffix),
+            },
+            sourceMap: true,
+          })
+        )
       )
     );
   },
