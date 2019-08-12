6278f16c03c40bc9fedeb640d7387e059afacd61
Akira Sudoh <asudoh@gmail.com>
2019-08-13 04:53:18 +0900

    chore(build): optimize for modern browsers (#86)
    This change mainly does the following change:
    
    - Re-introduce the bundled build
    - Make the ESM build target modern browsers; Applications that still
      support IE11 should re-transpile to use the ESM build
    - Fix the entry-point to include recently-introduced components
    
    Also this change removes `babel-plugin-supercall`, which was used for
    extending native `HTMLElement` in ES5 code. As of today, no component
    in this library extends native `HTMLElement` directly.
    
    Fixes #74.
    Fixes #75.

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,26 +1,10 @@
 'use strict';
 
-const nodeResolve = require('rollup-plugin-node-resolve');
-const string = require('rollup-plugin-string');
-const babel = require('rollup-plugin-babel');
-const commonjs = require('rollup-plugin-commonjs');
-const uglify = require('rollup-plugin-uglify');
+const { terser } = require('rollup-plugin-terser');
+
+const rollupConfig = require('./rollup.config.dev');
 
 module.exports = {
-  plugins: [
-    nodeResolve({
-      jsnext: true,
-      browser: true,
-      main: true,
-    }),
-    string({
-      include: '**/*.html',
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: [require.resolve('./babel-plugin-supercall'), 'external-helpers'],
-    }),
-    commonjs(),
-    uglify(),
-  ],
+  ...rollupConfig,
+  plugins: [...rollupConfig.plugins, terser()],
 };
