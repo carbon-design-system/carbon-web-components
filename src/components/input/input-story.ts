a72574109aab225fc59abac2f63418637f9b92ac
Cal Smith <calsmith@protonmail.com>
2019-08-13 17:41:22 -0400

    feat: update story to use types

--- src/components/input/input-story.ts
+++ src/components/input/input-story.ts
@@ -1,9 +1,16 @@
 import { html } from 'lit-element';
 import { storiesOf } from '@storybook/polymer';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { withKnobs, boolean, text, select } from '@storybook/addon-knobs';
 import './input';
 import '../form/form-item';
+import { INPUT_TYPE } from './input';
+
+const inputTypes = {};
+
+for (const [key, value] of Object.entries(INPUT_TYPE)) {
+  inputTypes[key.toLowerCase()] = value;
+}
 
 const createProps = () => ({
   disabled: boolean('Disabled (disabled)', false),
@@ -11,16 +18,18 @@ const createProps = () => ({
   placeholder: text('Placeholder text (placeholder)', 'Optional placeholder text'),
   invalid: boolean('Invalid (invalid)', false),
   onInput: action('input'),
+  type: select('Input type (type)', inputTypes, INPUT_TYPE.TEXT)
 });
 
 storiesOf('Input', module)
   .addDecorator(withKnobs)
   .add('Default', () => {
-    const { disabled, value, placeholder, invalid, onInput } = createProps();
+    const { disabled, value, placeholder, invalid, type, onInput } = createProps();
     return html`
       <bx-input
         ?disabled=${disabled}
         value="${value}"
+        type="${type}"
         placeholder="${placeholder}"
         ?invalid="${invalid}"
         @input="${onInput}"
@@ -30,10 +39,10 @@ storiesOf('Input', module)
   .add('Form item', () => {
     const { disabled, value, placeholder, invalid, onInput } = createProps();
     return html`
-      <bx-form-item ?invalid="${invalid}">
+      <bx-form-item ?invalid="${invalid}" ?disabled="${disabled}">
         <span slot="label">Label text</span>
         <span slot="help-text">Optional helper text</span>
-        <bx-input ?disabled=${disabled} value="${value}" placeholder="${placeholder}" @input="${onInput}"></bx-input>
+        <bx-input value="${value}" placeholder="${placeholder}" @input="${onInput}"></bx-input>
         <span slot="validation">Something isn't right</span>
       </bx-form-item>
     `;
