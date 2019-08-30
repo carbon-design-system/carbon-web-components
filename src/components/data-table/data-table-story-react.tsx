9afb6e014ccc8232269a9f9c499011e293442f53
Akira Sudoh <asudoh@gmail.com>
2019-08-30 19:45:22 +0900

    docs(data-table): add React story for pagination

--- src/components/data-table/data-table-story-react.tsx
+++ src/components/data-table/data-table-story-react.tsx
@@ -6,6 +6,12 @@ import { withKnobs, boolean, select } from '@storybook/addon-knobs';
 // Below path will be there when an application installs `carbon-custom-elements` package.
 // In our dev env, we auto-generate the file and re-map below path to to point to the genrated file.
 // @ts-ignore
+import BXPagination from 'carbon-custom-elements/es/components-react/pagination/pagination';
+// @ts-ignore
+import BXPageSizesSelect from 'carbon-custom-elements/es/components-react/pagination/page-sizes-select';
+// @ts-ignore
+import BXPagesSelect from 'carbon-custom-elements/es/components-react/pagination/pages-select';
+// @ts-ignore
 import BXTable, { TABLE_SIZE } from 'carbon-custom-elements/es/components-react/data-table/table';
 // @ts-ignore
 import BXTableHead from 'carbon-custom-elements/es/components-react/data-table/table-head';
@@ -19,7 +25,7 @@ import BXTableBody from 'carbon-custom-elements/es/components-react/data-table/t
 import BXTableRow from 'carbon-custom-elements/es/components-react/data-table/table-row';
 // @ts-ignore
 import BXTableCell from 'carbon-custom-elements/es/components-react/data-table/table-cell';
-import { rows as demoRows, columns as demoColumns, sortInfo as demoSortInfo } from './stories/data';
+import { rows as demoRows, rowsMany as demoRowsMany, columns as demoColumns, sortInfo as demoSortInfo } from './stories/data';
 import { TDemoTableColumn, TDemoTableRow, TDemoSortInfo } from './stories/types';
 
 /**
@@ -42,9 +48,11 @@ const BXCEDemoDataTable = ({
   collator,
   columns,
   hasSelection,
+  pageSize: propPageSize,
   rows: propRows,
   size,
   sortInfo: propSortInfo,
+  start: propStart,
   zebra,
   onChangeSelection,
   onChangeSelectionAll,
@@ -54,9 +62,11 @@ const BXCEDemoDataTable = ({
   collator?: Intl.Collator;
   columns: TDemoTableColumn[];
   hasSelection?: boolean;
+  pageSize?: number;
   rows: TDemoTableRow[];
   size?: TABLE_SIZE;
   sortInfo: TDemoSortInfo;
+  start?: number;
   zebra?: boolean;
   onChangeSelection?: (event: CustomEvent) => void;
   onChangeSelectionAll?: (event: CustomEvent) => void;
@@ -71,8 +81,10 @@ const BXCEDemoDataTable = ({
   );
   const elementId = id || uniqueId;
 
+  const [pageSize, setPageSize] = useState(propPageSize);
   const [rows, setRows] = useState(propRows);
   const [sortInfo, setSortInfo] = useState(propSortInfo);
+  const [start, setStart] = useState(propStart);
 
   const { columnId: sortColumnId, direction: sortDirection } = sortInfo;
   const selectionAllName = !hasSelection ? undefined : `__bx-ce-demo-data-table_select-all_${elementId}`;
@@ -92,6 +104,10 @@ const BXCEDemoDataTable = ({
     sortDirection === TABLE_SORT_DIRECTION.NONE
       ? rows!.slice()
       : rows!.slice().sort((lhs, rhs) => collationFactors[sortDirection] * compare(lhs[sortColumnId!], rhs[sortColumnId!]));
+
+  const handleChangePageSize = useCallback(({ detail }: CustomEvent) => {
+    setPageSize(detail.value);
+  }, []);
 
   const handleChangeSelection = useCallback(
     (event: CustomEvent) => {
@@ -146,50 +162,72 @@ const BXCEDemoDataTable = ({
     [propSortInfo, onSort]
   );
 
+  const handleChangeStart = useCallback(({ detail }: CustomEvent) => {
+    setStart(detail.start);
+  }, []);
+
+  const pagination =
+    typeof pageSize === 'undefined' ? (
+      undefined
+    ) : (
+      <BXPagination page-size={pageSize} start={start} total={rows!.length} onAfterChangeCurrent={handleChangeStart}>
+        <BXPageSizesSelect slot="page-sizes-select" onAfterChange={handleChangePageSize}>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </BXPageSizesSelect>
+        <BXPagesSelect />
+      </BXPagination>
+    );
+
   return (
-    <BXTable size={size}>
-      <BXTableHead>
-        <BXTableHeaderRow
-          selected={selectedAll}
-          selectionName={selectionAllName}
-          selectionValue={selectionAllName}
-          onBeforeChangeSelection={handleChangeSelectionAll}>
-          {columns.map(({ id: columnId, sortCycle, title }) => {
-            const sortDirectionForThisCell = sortCycle && (columnId === sortColumnId ? sortDirection : TABLE_SORT_DIRECTION.NONE);
+    <div>
+      <BXTable size={size}>
+        <BXTableHead>
+          <BXTableHeaderRow
+            selected={selectedAll}
+            selectionName={selectionAllName}
+            selectionValue={selectionAllName}
+            onBeforeChangeSelection={handleChangeSelectionAll}>
+            {columns.map(({ id: columnId, sortCycle, title }) => {
+              const sortDirectionForThisCell =
+                sortCycle && (columnId === sortColumnId ? sortDirection : TABLE_SORT_DIRECTION.NONE);
+              return (
+                <BXTableHeaderCell
+                  key={columnId}
+                  sortCycle={sortCycle}
+                  sort-direction={sortDirectionForThisCell}
+                  data-column-id={columnId}
+                  onBeforeSort={handleChangeSort}>
+                  {title}
+                </BXTableHeaderCell>
+              );
+            })}
+          </BXTableHeaderRow>
+        </BXTableHead>
+        <BXTableBody zebra={zebra}>
+          {sortedRows.slice(start, start! + pageSize!).map(row => {
+            const { id: rowId, selected } = row;
+            const selectionName = !hasSelection ? undefined : `__bx-ce-demo-data-table_${elementId}_${rowId}`;
+            const selectionValue = !hasSelection ? undefined : 'selected';
             return (
-              <BXTableHeaderCell
-                key={columnId}
-                sortCycle={sortCycle}
-                sort-direction={sortDirectionForThisCell}
-                data-column-id={columnId}
-                onBeforeSort={handleChangeSort}>
-                {title}
-              </BXTableHeaderCell>
+              <BXTableRow
+                key={rowId}
+                selected={hasSelection && selected}
+                selectionName={selectionName}
+                selectionValue={selectionValue}
+                data-row-id={rowId}
+                onBeforeChangeSelection={handleChangeSelection}>
+                {columns.map(({ id: columnId }) => (
+                  <BXTableCell key={columnId}>{row[columnId]}</BXTableCell>
+                ))}
+              </BXTableRow>
             );
           })}
-        </BXTableHeaderRow>
-      </BXTableHead>
-      <BXTableBody zebra={zebra}>
-        {sortedRows.map(row => {
-          const { id: rowId, selected } = row;
-          const selectionName = !hasSelection ? undefined : `__bx-ce-demo-data-table_${elementId}_${rowId}`;
-          const selectionValue = !hasSelection ? undefined : 'selected';
-          return (
-            <BXTableRow
-              key={rowId}
-              selected={hasSelection && selected}
-              selectionName={selectionName}
-              selectionValue={selectionValue}
-              data-row-id={rowId}
-              onBeforeChangeSelection={handleChangeSelection}>
-              {columns.map(({ id: columnId }) => (
-                <BXTableCell key={columnId}>{row[columnId]}</BXTableCell>
-              ))}
-            </BXTableRow>
-          );
-        })}
-      </BXTableBody>
-    </BXTable>
+        </BXTableBody>
+      </BXTable>
+      {pagination}
+    </div>
   );
 };
 
@@ -234,9 +272,19 @@ BXCEDemoDataTable.propTypes = {
   hasSelection: PropTypes.bool,
 
   /**
+   * Number of items per page.
+   */
+  pageSize: PropTypes.number,
+
+  /**
    * `true` if the the table should use the compact version of the UI. Corresponds to the attribute with the same name.
    */
   size: PropTypes.string,
+
+  /**
+   * The row number where current page start with, index that starts with zero. Corresponds to the attribute with the same name.
+   */
+  start: PropTypes.number,
 
   /**
    * `true` if the zebra stripe should be shown.
@@ -263,6 +311,7 @@ BXCEDemoDataTable.defaultProps = {
   collator: new Intl.Collator(),
   hasSelection: false,
   size: TABLE_SIZE.REGULAR,
+  start: 0,
 };
 
 const sizes = {
@@ -361,6 +410,46 @@ storiesOf('Data table', module)
           sortInfo={demoSortInfo}
           hasSelection={hasSelection}
           size={size}
+          zebra={zebra}
+          onChangeSelection={beforeChangeSelectionHandler}
+          onChangeSelectionAll={beforeChangeSelectionHandler}
+          onSort={beforeChangeSortHandler}
+        />
+      </>
+    );
+  })
+  .add('Sortable with pagination', () => {
+    const { hasSelection, size, zebra, disableChangeSelection, disableChangeSort } = createProps({ sortable: true });
+    const beforeChangeSelectionAction = action('onBeforeChangeSelection');
+    const beforeChangeSelectionAllAction = action('onBeforeChangeSelection (for selecting all rows)');
+    const beforeChangeSelectionHandler = (event: CustomEvent) => {
+      if (event.type === 'bx-table-change-selection-all') {
+        beforeChangeSelectionAllAction(event);
+      } else {
+        beforeChangeSelectionAction(event);
+      }
+      if (disableChangeSelection) {
+        event.preventDefault();
+      }
+    };
+    const beforeChangeSortAction = action('onBeforeSort');
+    const beforeChangeSortHandler = (event: CustomEvent) => {
+      beforeChangeSortAction(event);
+      if (disableChangeSort) {
+        event.preventDefault();
+      }
+    };
+    return (
+      <>
+        {/* Refer to <bx-ce-demo-data-table> implementation at the top for details */}
+        <BXCEDemoDataTable
+          columns={demoColumns}
+          rows={demoRowsMany}
+          sortInfo={demoSortInfo}
+          hasSelection={hasSelection}
+          pageSize={5}
+          size={size}
+          start={0}
           zebra={zebra}
           onChangeSelection={beforeChangeSelectionHandler}
           onChangeSelectionAll={beforeChangeSelectionHandler}
