9be8b6d689756c2368253bac63beab8e327b575d
Akira Sudoh <asudoh@gmail.com>
2019-09-17 21:30:24 +0900

    docs(components): introduce CodeSandbox examples (#114)
    This change introduces CodeSandbox examples for:
    
    * Basic usage
    * Angular
    * React
    * Vue
    * IE
    
    This change also updates `README.md` to link to the CodeSandbox
    examples.

---
+++ examples/codesandbox/angular/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ *
+ * Copyright IBM Corp. 2019
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Component } from '@angular/core';
+import 'carbon-custom-elements/es/components/dropdown/dropdown';
+import 'carbon-custom-elements/es/components/dropdown/dropdown-item';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {} // eslint-disable-line import/prefer-default-export
