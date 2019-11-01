eb6fb8e4faaef09bf6cf9567163e2f38ce0e46e2
Akira Sudoh <asudoh@gmail.com>
2019-11-02 08:34:55 +0900

    Update src/components/notification/notification-story-angular.ts
    Co-Authored-By: emyarod <emyarod@users.noreply.github.com>

--- src/components/notification/notification-story-angular.ts
+++ src/components/notification/notification-story-angular.ts
@@ -125,7 +125,7 @@ They usually appear at the top of the primary content area.
     {
       docs: {
         storyDescription: `
-Toasts are a non-modal, time-based window elements used to display short messages;
+Toasts are non-modal, time-based window elements used to display short messages;
 they usually appear at the bottom of the screen and disappear after a few seconds.
       `,
       },
