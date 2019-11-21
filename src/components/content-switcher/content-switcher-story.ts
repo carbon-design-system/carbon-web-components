ff900f4829ca5a9ef6b920e2b1a3031a570e8f59
Akira Sudoh <asudoh@gmail.com>
2019-11-22 03:28:46 +0900

    chore(content-switcher): move to CSF for lit

--- src/components/content-switcher/content-switcher-story.ts
+++ src/components/content-switcher/content-switcher-story.ts
@@ -8,44 +8,53 @@
  */
 
 import { html } from 'lit-element';
-import { storiesOf } from '@storybook/polymer';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs';
 import './content-switcher';
 import './content-switcher-item';
 
-const createProps = () => ({
-  disabled: boolean('Disabled (disabled)', false),
-  value: text('The value of the selected item (value)', ''),
-  disableSelection: boolean(
-    'Disable user-initiated selection change (Call event.preventDefault() in bx-content-switcher-beingselected event)',
-    false
-  ),
-});
+export const defaultStory = ({ parameters }) => {
+  const { disabled, value, disableSelection } =
+    (parameters.props && parameters.props['bx-content-switcher']) || ({} as typeof parameters.props['bx-content-switcher']);
+  const beforeSelectedAction = action('bx-content-switcher-beingselected');
+  const handleBeforeSelected = (event: CustomEvent) => {
+    beforeSelectedAction(event);
+    if (disableSelection) {
+      event.preventDefault();
+    }
+  };
+  return html`
+    <bx-content-switcher
+      ?disabled="${disabled}"
+      value="${value}"
+      @bx-content-switcher-beingselected="${handleBeforeSelected}"
+      @bx-content-switcher-selected="${action('bx-content-switcher-selected')}"
+    >
+      <bx-content-switcher-item value="all">Option 1</bx-content-switcher-item>
+      <bx-content-switcher-item value="cloudFoundry" disabled>Option 2</bx-content-switcher-item>
+      <bx-content-switcher-item value="staging">Option 3</bx-content-switcher-item>
+      <bx-content-switcher-item value="dea">Option 4</bx-content-switcher-item>
+      <bx-content-switcher-item value="router">Option 5</bx-content-switcher-item>
+    </bx-content-switcher>
+  `;
+};
 
-storiesOf('Content switcher', module)
-  .addDecorator(withKnobs)
-  .add('Default', () => {
-    const { disabled, value, disableSelection } = createProps();
-    const beforeSelectedAction = action('bx-content-switcher-beingselected');
-    const handleBeforeSelected = (event: CustomEvent) => {
-      beforeSelectedAction(event);
-      if (disableSelection) {
-        event.preventDefault();
-      }
-    };
-    return html`
-      <bx-content-switcher
-        ?disabled=${disabled}
-        value=${value}
-        @bx-content-switcher-beingselected=${handleBeforeSelected}
-        @bx-content-switcher-selected=${action('bx-content-switcher-selected')}
-      >
-        <bx-content-switcher-item value="all">Option 1</bx-content-switcher-item>
-        <bx-content-switcher-item value="cloudFoundry" disabled>Option 2</bx-content-switcher-item>
-        <bx-content-switcher-item value="staging">Option 3</bx-content-switcher-item>
-        <bx-content-switcher-item value="dea">Option 4</bx-content-switcher-item>
-        <bx-content-switcher-item value="router">Option 5</bx-content-switcher-item>
-      </bx-content-switcher>
-    `;
-  });
+defaultStory.story = {
+  name: 'Default',
+};
+
+export default {
+  title: 'Content switcher',
+  parameters: {
+    knobs: {
+      'bx-content-switcher': () => ({
+        disabled: boolean('Disabled (disabled)', false),
+        value: text('The value of the selected item (value)', ''),
+        disableSelection: boolean(
+          'Disable user-initiated selection change (Call event.preventDefault() in bx-content-switcher-beingselected event)',
+          false
+        ),
+      }),
+    },
+  },
+};
--- src/components/tabs/tabs-story.ts
+++ src/components/tabs/tabs-story.ts
@@ -8,87 +8,96 @@
  */
 
 import { html } from 'lit-element';
-import { storiesOf } from '@storybook/polymer';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs';
 import './tabs';
 import './tab';
 import styles from './tabs-story.scss';
 
-const createProps = () => ({
-  disabled: boolean('Disabled (disabled)', false),
-  triggerContent: text('The default content of the trigger button for narrow screen (trigger-content)', 'Select an item'),
-  value: text('The value of the selected item (value)', 'staging'),
-  disableSelection: boolean(
-    'Disable user-initiated selection change (Call event.preventDefault() in bx-content-switcher-beingselected event)',
-    false
-  ),
-});
-
-storiesOf('Tabs', module)
-  .addDecorator(withKnobs)
-  .add('Default', () => {
-    const { disabled, triggerContent, value, disableSelection } = createProps();
-    const beforeSelectedAction = action('bx-content-switcher-beingselected');
-    const handleBeforeSelected = (event: CustomEvent) => {
-      beforeSelectedAction(event);
-      if (disableSelection) {
-        event.preventDefault();
-      }
-    };
-    return html`
-      <style>
-        ${styles}
-      </style>
-      <bx-tabs
-        ?disabled=${disabled}
-        trigger-content="${triggerContent}"
-        value=${value}
-        @bx-tabs-beingselected=${handleBeforeSelected}
-        @bx-tabs-selected=${action('bx-tabs-selected')}
-      >
-        <bx-tab id="tab-all" target="panel-all" value="all">Option 1</bx-tab>
-        <bx-tab id="tab-cloudFoundry" target="panel-cloudFoundry" disabled value="cloudFoundry">Option 2</bx-tab>
-        <bx-tab id="tab-staging" target="panel-staging" value="staging">Option 3</bx-tab>
-        <bx-tab id="tab-dea" target="panel-dea" value="dea">Option 4</bx-tab>
-        <bx-tab id="tab-router" target="panel-router" value="router">Option 5</bx-tab>
-      </bx-tabs>
-      <div class="bx-ce-demo-devenv--tab-panels">
-        <div id="panel-all" role="tabpanel" aria-labelledby="tab-all" hidden>
-          <h1>Content for option 1</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div id="panel-cloudFoundry" role="tabpanel" aria-labelledby="tab-cloudFoundry" hidden>
-          <h1>Content for option 2</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div id="panel-staging" role="tabpanel" aria-labelledby="tab-staging" hidden>
-          <h1>Content for option 3</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div id="panel-dea" role="tabpanel" aria-labelledby="tab-dea" hidden>
-          <h1>Content for option 4</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div id="panel-router" role="tabpanel" aria-labelledby="tab-router" hidden>
-          <h1>Content for option 5</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
+export const defaultStory = ({ parameters }) => {
+  const { disabled, triggerContent, value, disableSelection } =
+    (parameters.props && parameters.props['bx-tabs']) || ({} as typeof parameters.props['bx-tabs']);
+  const beforeSelectedAction = action('bx-tabs-beingselected');
+  const handleBeforeSelected = (event: CustomEvent) => {
+    beforeSelectedAction(event);
+    if (disableSelection) {
+      event.preventDefault();
+    }
+  };
+  return html`
+    <style>
+      ${styles}
+    </style>
+    <bx-tabs
+      ?disabled="${disabled}"
+      trigger-content="${triggerContent}"
+      value="${value}"
+      @bx-tabs-beingselected="${handleBeforeSelected}"
+      @bx-tabs-selected="${action('bx-tabs-selected')}"
+    >
+      <bx-tab id="tab-all" target="panel-all" value="all">Option 1</bx-tab>
+      <bx-tab id="tab-cloudFoundry" target="panel-cloudFoundry" disabled value="cloudFoundry">Option 2</bx-tab>
+      <bx-tab id="tab-staging" target="panel-staging" value="staging">Option 3</bx-tab>
+      <bx-tab id="tab-dea" target="panel-dea" value="dea">Option 4</bx-tab>
+      <bx-tab id="tab-router" target="panel-router" value="router">Option 5</bx-tab>
+    </bx-tabs>
+    <div class="bx-ce-demo-devenv--tab-panels">
+      <div id="panel-all" role="tabpanel" aria-labelledby="tab-all" hidden>
+        <h1>Content for option 1</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
       </div>
-    `;
-  });
+      <div id="panel-cloudFoundry" role="tabpanel" aria-labelledby="tab-cloudFoundry" hidden>
+        <h1>Content for option 2</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div id="panel-staging" role="tabpanel" aria-labelledby="tab-staging" hidden>
+        <h1>Content for option 3</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div id="panel-dea" role="tabpanel" aria-labelledby="tab-dea" hidden>
+        <h1>Content for option 4</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div id="panel-router" role="tabpanel" aria-labelledby="tab-router" hidden>
+        <h1>Content for option 5</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  `;
+};
+
+defaultStory.story = {
+  name: 'Default',
+};
+
+export default {
+  title: 'Tabs',
+  parameters: {
+    knobs: {
+      'bx-tabs': () => ({
+        disabled: boolean('Disabled (disabled)', false),
+        triggerContent: text('The default content of the trigger button for narrow screen (trigger-content)', 'Select an item'),
+        value: text('The value of the selected item (value)', 'staging'),
+        disableSelection: boolean(
+          'Disable user-initiated selection change (Call event.preventDefault() in bx-content-switcher-beingselected event)',
+          false
+        ),
+      }),
+    },
+  },
+};
