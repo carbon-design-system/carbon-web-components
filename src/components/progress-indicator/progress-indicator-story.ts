66bc4b903793c79395f05090d8f622e29234c9d6
Akira Sudoh <asudoh@gmail.com>
2019-11-22 17:41:28 +0900

    chore(devenv): convert lit story to CSF

--- src/components/progress-indicator/progress-indicator-story.ts
+++ src/components/progress-indicator/progress-indicator-story.ts
@@ -9,57 +9,66 @@
 
 import { ifDefined } from 'lit-html/directives/if-defined';
 import { html } from 'lit-element';
-import { storiesOf } from '@storybook/polymer';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs';
 import './progress-indicator';
 import './progress-step';
 
-const createProps = () => ({
-  vertical: boolean('Vertical (vertical)', false),
-});
+export const defaultStory = ({ parameters }) => {
+  const { vertical } =
+    (parameters.props && parameters.props['bx-progress-indicator']) || ({} as typeof parameters.props['bx-progress-indicator']);
+  const { iconLabel, labelText, secondaryLabelText } =
+    (parameters.props && parameters.props['bx-progress-step']) || ({} as typeof parameters.props['bx-progress-step']);
+  return html`
+    <bx-progress-indicator ?vertical="${vertical}">
+      <bx-progress-step
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        label-text="${ifDefined(!labelText ? undefined : labelText)}"
+        secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
+        state="invalid"
+      ></bx-progress-step>
+      <bx-progress-step
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        label-text="${ifDefined(!labelText ? undefined : labelText)}"
+        secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
+        state="complete"
+      ></bx-progress-step>
+      <bx-progress-step
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        label-text="${ifDefined(!labelText ? undefined : labelText)}"
+        secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
+        state="current"
+      ></bx-progress-step>
+      <bx-progress-step
+        disabled
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        label-text="${ifDefined(!labelText ? undefined : labelText)}"
+        secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
+      ></bx-progress-step>
+      <bx-progress-step
+        icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
+        label-text="${ifDefined(!labelText ? undefined : labelText)}"
+        secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
+      ></bx-progress-step>
+    </bx-progress-indicator>
+  `;
+};
 
-const createStepProps = () => ({
-  iconLabel: text('Icon label (icon-label)', ''),
-  labelText: text('Primary label text (label-text)', 'Label'),
-  secondaryLabelText: text('Secondary label text (secondary-label-text)', 'Secondary label'),
-});
+defaultStory.story = {
+  name: 'Default',
+};
 
-storiesOf('Progress indicator', module)
-  .addDecorator(withKnobs)
-  .add('Default', () => {
-    const { vertical } = createProps();
-    const { iconLabel, labelText, secondaryLabelText } = createStepProps();
-    return html`
-      <bx-progress-indicator ?vertical="${vertical}">
-        <bx-progress-step
-          icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
-          label-text="${ifDefined(!labelText ? undefined : labelText)}"
-          secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
-          state="invalid"
-        ></bx-progress-step>
-        <bx-progress-step
-          icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
-          label-text="${ifDefined(!labelText ? undefined : labelText)}"
-          secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
-          state="complete"
-        ></bx-progress-step>
-        <bx-progress-step
-          icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
-          label-text="${ifDefined(!labelText ? undefined : labelText)}"
-          secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
-          state="current"
-        ></bx-progress-step>
-        <bx-progress-step
-          disabled
-          icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
-          label-text="${ifDefined(!labelText ? undefined : labelText)}"
-          secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
-        ></bx-progress-step>
-        <bx-progress-step
-          icon-label="${ifDefined(!iconLabel ? undefined : iconLabel)}"
-          label-text="${ifDefined(!labelText ? undefined : labelText)}"
-          secondary-label-text="${ifDefined(!secondaryLabelText ? undefined : secondaryLabelText)}"
-        ></bx-progress-step>
-      </bx-progress-indicator>
-    `;
-  });
+export default {
+  title: 'Progress indicator',
+  parameters: {
+    knobs: {
+      'bx-progress-indicator': () => ({
+        vertical: boolean('Vertical (vertical)', false),
+      }),
+      'bx-progress-step': () => ({
+        iconLabel: text('Icon label (icon-label)', ''),
+        labelText: text('Primary label text (label-text)', 'Label'),
+        secondaryLabelText: text('Secondary label text (secondary-label-text)', 'Secondary label'),
+      }),
+    },
+  },
+};
