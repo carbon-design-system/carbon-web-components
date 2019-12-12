6da4f65aea03b33ee79de5467480cec72aec0368
Abdón Rodríguez Davila <a@abdonrd.com>
2019-12-12 09:41:39 +0100

    chore: replace lodash.findLast with lodash-es

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -8,7 +8,7 @@
  */
 
 import settings from 'carbon-components/es/globals/js/settings';
-import findLast from 'lodash.findlast';
+import findLast from 'lodash-es/findLast';
 import classnames from 'classnames';
 import { html, property, query, customElement, LitElement } from 'lit-element';
 import HostListener from '../../globals/decorators/host-listener';
