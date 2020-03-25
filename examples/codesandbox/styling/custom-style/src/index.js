e93ac3d80f472a269efc4da37701c49fd66c6f1f
Akira Sudoh <asudoh@gmail.com>
2020-03-26 02:06:01 +0900

    docs(styling): update example with extended style
    Change the example of derived class with additional styles, to a more
    efficient way.

--- examples/codesandbox/styling/custom-style/src/index.js
+++ examples/codesandbox/styling/custom-style/src/index.js
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019
+ * Copyright IBM Corp. 2019, 2020
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -14,11 +14,13 @@ class MyDropdown extends BXDropdown {}
 
 // We seem to have hit something similar to: https://github.com/codesandbox/codesandbox-client/issues/1935
 // Custom CSS to enforce `field-02` (light) style of the dropdown
-MyDropdown.styles = css`
-  ${BXDropdown.styles}
-  .bx--list-box {
-    background-color: white;
-  }
-`;
+MyDropdown.styles = [
+  BXDropdown.styles,
+  css`
+    .bx--list-box {
+      background-color: white;
+    }
+  `,
+];
 
 customElements.define('my-dropdown', MyDropdown);
