7a7fcf1bfb9bf56d8e56b71cd77e2583092605b3
Akira Sudoh <asudoh@gmail.com>
2020-03-26 13:41:43 +0900

    fix(slider): support min/max with keyboard gesture
    This change makes `<bx-slider>` as well as `<bx-slider-input>` support
    `min`/`max` for changing the value by arrow keys.
    
    Also:
    
    * Fixes an issue where the slider's value may be `NaN` when
      `<bx-slider-input>` sends its value to `<bx-slider>`.
    * Ensures that the new value snaps on steps with arrow keys.

--- src/components/slider/slider-input.ts
+++ src/components/slider/slider-input.ts
@@ -24,6 +24,21 @@ const { prefix } = settings;
 @customElement(`${prefix}-slider-input`)
 class BXSliderInput extends FocusMixin(LitElement) {
   /**
+   * The internal value of `max` property.
+   */
+  private _max = '100';
+
+  /**
+   * The internal value of `min` property.
+   */
+  private _min = '0';
+
+  /**
+   * The internal value of `step` property.
+   */
+  private _step = '1';
+
+  /**
    * Handles `change` event to fire a normalized custom event.
    */
   private _handleChange({ target }: Event) {
@@ -32,7 +47,7 @@ class BXSliderInput extends FocusMixin(LitElement) {
         bubbles: true,
         composed: true,
         detail: {
-          value: (target as HTMLInputElement).value,
+          value: Number((target as HTMLInputElement).value),
         },
       })
     );
@@ -47,7 +62,7 @@ class BXSliderInput extends FocusMixin(LitElement) {
         bubbles: true,
         composed: true,
         detail: {
-          value: (target as HTMLInputElement).value,
+          value: Number((target as HTMLInputElement).value),
           intermediate: true,
         },
       })
@@ -67,10 +82,52 @@ class BXSliderInput extends FocusMixin(LitElement) {
   light = false;
 
   /**
+   * The maximum value.
+   */
+  @property({ type: Number, reflect: true })
+  get max() {
+    return this._max.toString();
+  }
+
+  set max(value) {
+    const { max: oldMax } = this;
+    this._max = value;
+    this.requestUpdate('max', oldMax);
+  }
+
+  /**
+   * The minimum value.
+   */
+  @property({ type: Number, reflect: true })
+  get min() {
+    return this._min.toString();
+  }
+
+  set min(value) {
+    const { min: oldMin } = this;
+    this._min = value;
+    this.requestUpdate('min', oldMin);
+  }
+
+  /**
+   * The snapping step of the value.
+   */
+  @property({ type: Number, reflect: true })
+  get step() {
+    return this._step.toString();
+  }
+
+  set step(value) {
+    const { step: oldStep } = this;
+    this._step = value;
+    this.requestUpdate('step', oldStep);
+  }
+
+  /**
    * The type of the `<input>`.
    */
   @property()
-  type!: string;
+  type = 'number';
 
   /**
    * The value.
@@ -83,7 +140,7 @@ class BXSliderInput extends FocusMixin(LitElement) {
   }
 
   render() {
-    const { disabled, light, type, value, _handleChange: handleChange, _handleInput: handleInput } = this;
+    const { disabled, light, max, min, step, type, value, _handleChange: handleChange, _handleInput: handleInput } = this;
     // NOTE: Our React variant has an option to add `invalid` option here,
     // but there doesn't seem a corresponding style to the thumb.
     // Because of that, in addition to the mininum/maximum constraint enforced,
@@ -98,6 +155,9 @@ class BXSliderInput extends FocusMixin(LitElement) {
         ?disabled="${disabled}"
         type="${ifNonNull(type)}"
         class="${classes}"
+        max="${max}"
+        min="${min}"
+        step="${step}"
         .value="${value}"
         @change="${handleChange}"
         @input="${handleInput}"
