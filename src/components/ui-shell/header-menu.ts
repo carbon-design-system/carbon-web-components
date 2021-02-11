591f1da9de38a64c1f82598a61f736be55d830dc
Anna Wen <54281166+annawen1@users.noreply.github.com>
2021-02-11 10:05:23 -0500

    fix(header-menu): remove menu role for accessbility

--- src/components/ui-shell/header-menu.ts
+++ src/components/ui-shell/header-menu.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2020
+ * Copyright IBM Corp. 2019, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -130,7 +130,7 @@ class BXHeaderMenu extends HostListenerMixin(FocusMixin(LitElement)) {
       >
         ${triggerContent}${ChevronDownGlyph({ part: 'trigger-icon', class: `${prefix}--header__menu-arrow` })}
       </a>
-      <ul role="menu" part="menu-body" class="${prefix}--header__menu" aria-label="${ifDefined(menuLabel)}">
+      <ul part="menu-body" class="${prefix}--header__menu" aria-label="${ifDefined(menuLabel)}">
         <slot></slot>
       </ul>
     `;
