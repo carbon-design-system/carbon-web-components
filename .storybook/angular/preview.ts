66858364d87f7cef2e50f221ea5c9bb7a07ff87b
Akira Sudoh <asudoh@gmail.com>
2021-03-06 10:26:33 +0900

    chore(devenv): fix Storybook title (#601)
    Fixes a bug where the title of Storybook was not correctly shown.
    Also adds library version number to the title.

--- .storybook/angular/preview.ts
+++ .storybook/angular/preview.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -14,7 +14,7 @@ import theme from './theme';
 
 addons.setConfig({
   showRoots: true,
-  theme: theme,
+  theme,
 });
 
 const SORT_ORDER = ['introduction-welcome--page', 'introduction-form-paticipation--page'];
@@ -32,6 +32,7 @@ export const parameters = {
       }
       return 0;
     },
+    theme,
   },
 };
 
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -22,7 +22,7 @@ addons.getChannel().on(CURRENT_THEME, theme => {
 
 addons.setConfig({
   showRoots: true,
-  theme: theme,
+  theme,
 });
 
 const SORT_ORDER = ['introduction-welcome--page', 'introduction-form-paticipation--page', 'introduction-custom-styles--page'];
@@ -40,6 +40,7 @@ export const parameters = {
       }
       return 0;
     },
+    theme,
   },
 };
 
