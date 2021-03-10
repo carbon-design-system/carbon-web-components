e008e8ed62a82d45e33a3e7e70a1010f16ac75d8
Akira Sudoh <asudoh@gmail.com>
2021-03-10 19:03:21 +0900

    chore(devenv): fix JS error in the Docs tab of skeleton text
    Fixes JavaScript error in the Docs tab of the story of
    `<bx-skeleton-text>`. Also adds Docs tab code for
    `<bx-file-uploader>` and `<bx-select>`.
    
    Refs #602.

--- src/components/skeleton-text/skeleton-text-story.ts
+++ src/components/skeleton-text/skeleton-text-story.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2020
+ * Copyright IBM Corp. 2019, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -54,9 +54,6 @@ export default {
     ...storyDocs.parameters,
     percy: {
       skip: true,
-    },
-    docs: {
-      page: storyDocs,
     },
   },
 };
