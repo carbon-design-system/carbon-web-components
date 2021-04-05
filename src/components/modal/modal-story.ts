cba1ed4013a26c3fc2f7e49856cf95d4d193fc82
Akira Sudoh <asudoh@gmail.com>
2021-04-06 00:56:55 +0900

    feat(modal): support one/three buttons (#613)
    Also adds support for button separator as well as modal footer-specific
    layout with a new component, `<bx-modal-footer-button>`.
    
    Refs #585.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/components/modal/modal-story.ts
+++ src/components/modal/modal-story.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2020
+ * Copyright IBM Corp. 2019, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -19,6 +19,7 @@ import './modal-heading';
 import './modal-label';
 import './modal-body';
 import './modal-footer';
+import './modal-footer-button';
 import styles from './modal-story.scss';
 import storyDocs from './modal-story.mdx';
 
@@ -46,14 +47,66 @@ export const Default = args => {
       </bx-modal-header>
       <bx-modal-body><p>Modal text description</p></bx-modal-body>
       <bx-modal-footer>
-        <bx-btn kind="secondary" data-modal-close>Cancel</bx-btn>
-        <bx-btn kind="primary">Save</bx-btn>
+        <bx-modal-footer-button kind="secondary" data-modal-close>Cancel</bx-modal-footer-button>
+        <bx-modal-footer-button kind="primary">Save</bx-modal-footer-button>
       </bx-modal-footer>
     </bx-modal>
   `;
 };
 
 Default.storyName = 'Default';
+
+export const SingleButton = args => {
+  const { open, size, disableClose, onBeforeClose, onClose } = args?.['bx-modal'] ?? {};
+  const handleBeforeClose = (event: CustomEvent) => {
+    onBeforeClose(event);
+    if (disableClose) {
+      event.preventDefault();
+    }
+  };
+  return html`
+    <bx-modal ?open="${open}" size="${ifNonNull(size)}" @bx-modal-beingclosed=${handleBeforeClose} @bx-modal-closed=${onClose}>
+      <bx-modal-header>
+        <bx-modal-close-button></bx-modal-close-button>
+        <bx-modal-label>Label (Optional)</bx-modal-label>
+        <bx-modal-heading>Modal Title</bx-modal-heading>
+      </bx-modal-header>
+      <bx-modal-body><p>Modal text description</p></bx-modal-body>
+      <bx-modal-footer>
+        <bx-modal-footer-button kind="primary">Save</bx-modal-footer-button>
+      </bx-modal-footer>
+    </bx-modal>
+  `;
+};
+
+SingleButton.storyName = 'Single button';
+
+export const ThreeButtons = args => {
+  const { open, size, disableClose, onBeforeClose, onClose } = args?.['bx-modal'] ?? {};
+  const handleBeforeClose = (event: CustomEvent) => {
+    onBeforeClose(event);
+    if (disableClose) {
+      event.preventDefault();
+    }
+  };
+  return html`
+    <bx-modal ?open="${open}" size="${ifNonNull(size)}" @bx-modal-beingclosed=${handleBeforeClose} @bx-modal-closed=${onClose}>
+      <bx-modal-header>
+        <bx-modal-close-button></bx-modal-close-button>
+        <bx-modal-label>Label (Optional)</bx-modal-label>
+        <bx-modal-heading>Modal Title</bx-modal-heading>
+      </bx-modal-header>
+      <bx-modal-body><p>Modal text description</p></bx-modal-body>
+      <bx-modal-footer>
+        <bx-modal-footer-button kind="secondary">Apply</bx-modal-footer-button>
+        <bx-modal-footer-button kind="secondary" data-modal-close>Cancel</bx-modal-footer-button>
+        <bx-modal-footer-button kind="primary">Save</bx-modal-footer-button>
+      </bx-modal-footer>
+    </bx-modal>
+  `;
+};
+
+ThreeButtons.storyName = 'Three buttons';
 
 export default {
   title: 'Components/Modal',
