4f67214eba6178b16bf534db04b8b6cac1b329b8
kennylam <kennylam@protonmail.com>
2021-05-11 09:50:09 -0400

    chore(deps): upgrade Carbon to 10.34.0 (#636)

--- src/components/search/search.ts
+++ src/components/search/search.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2020
+ * Copyright IBM Corp. 2019, 2021
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -154,7 +154,7 @@ class BXSearch extends FocusMixin(LitElement) {
     return html`
       ${Search16({
         part: 'search-icon',
-        class: `${prefix}--search-magnifier`,
+        class: `${prefix}--search-magnifier-icon`,
         role: 'img',
       })}
       <label for="input" part="label-text" class="${prefix}--label">
