577cf1e59f86ab5bb3775c5315a2789480e5daff
IgnacioBecerra <i1becerr@ucsd.edu>
2021-12-01 02:44:34 -0800

    chore(format): ran prettier)

--- src/components/combo-box/combo-box.ts
+++ src/components/combo-box/combo-box.ts
@@ -188,8 +188,7 @@ class BXComboBox extends BXDropdown {
         aria-label="${inputLabel}"
         aria-controls="menu-body"
         aria-autocomplete="list"
-        @input=${handleInput}
-      />
+        @input=${handleInput} />
     `;
   }
 
@@ -203,8 +202,7 @@ class BXComboBox extends BXDropdown {
             role="button"
             class="${prefix}--list-box__selection"
             tabindex="0"
-            title="${clearSelectionLabel}"
-          >
+            title="${clearSelectionLabel}">
             ${Close16({ 'aria-label': clearSelectionLabel })}
           </div>
         `;
