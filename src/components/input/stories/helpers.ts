b92c180773e28843c6e81842278d75353328829d
emyarod <afw5059@gmail.com>
2022-02-09 14:38:19 -0600

    docs(input): conditionally display password visibility knob

--- src/components/input/stories/helpers.ts
+++ src/components/input/stories/helpers.ts
@@ -29,16 +29,22 @@ const colorSchemes = {
   [`Light (${INPUT_COLOR_SCHEME.LIGHT})`]: INPUT_COLOR_SCHEME.LIGHT,
 };
 
-const createProps = ({ boolean, textNonEmpty, select }) => ({
-  colorScheme: select('Color scheme (color-scheme)', colorSchemes, null),
-  disabled: boolean('Disabled (disabled)', false),
-  value: textNonEmpty('Input value (value)', ''),
-  placeholder: textNonEmpty('Placeholder text (placeholder)', 'Optional placeholder text'),
-  invalid: boolean('Invalid (invalid)', false),
-  onInput: action('input'),
-  showPasswordVisibilityToggle: boolean('Show password visibility toggle (show-password-visibility-toggle)', false),
-  size: select('Input size (size)', sizes, INPUT_SIZE.REGULAR),
-  type: select('Input type (type)', inputTypes, INPUT_TYPE.TEXT),
-});
+const createProps = ({ boolean, textNonEmpty, select }) => {
+  const type = select('Input type (type)', inputTypes, INPUT_TYPE.TEXT);
+  return {
+    colorScheme: select('Color scheme (color-scheme)', colorSchemes, null),
+    disabled: boolean('Disabled (disabled)', false),
+    value: textNonEmpty('Input value (value)', ''),
+    placeholder: textNonEmpty('Placeholder text (placeholder)', 'Optional placeholder text'),
+    invalid: boolean('Invalid (invalid)', false),
+    onInput: action('input'),
+    showPasswordVisibilityToggle:
+      type === INPUT_TYPE.TEXT || type === INPUT_TYPE.PASSWORD
+        ? boolean('Show password visibility toggle (show-password-visibility-toggle)', false)
+        : null,
+    size: select('Input size (size)', sizes, INPUT_SIZE.REGULAR),
+    type,
+  };
+};
 
 export default createProps;
