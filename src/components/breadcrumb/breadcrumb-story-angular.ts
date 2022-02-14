cab6e5415ccfbdd815bea7f71cd30a5ca822eac9
Ariella Gilmore <ariellalgilmore@gmail.com>
2022-02-14 12:07:41 -0700

    feat(breadcrumb): introduce overflow menu (#948)

--- src/components/breadcrumb/breadcrumb-story-angular.ts
+++ src/components/breadcrumb/breadcrumb-story-angular.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2020
+ * Copyright IBM Corp. 2019, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -35,4 +35,28 @@ export default Object.assign(baseStory, {
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }),
   ],
+});
+
+export const withOverflowMenu = () => ({
+  template: `
+    <bx-breadcrumb>
+      <bx-breadcrumb-item>
+        <bx-breadcrumb-link href="/#">Breadcrumb 1</bx-breadcrumb-link>
+      </bx-breadcrumb-item>
+      <bx-breadcrumb-item>
+        <bx-breadcrumb-link href="/#">Breadcrumb 2</bx-breadcrumb-link>
+      </bx-breadcrumb-item>
+      <bx-breadcrumb-item>
+        <bx-breadcrumb-overflow-menu>
+          <bx-overflow-menu-body>
+            <bx-overflow-menu-item>Option 1</bx-overflow-menu-item>
+            <bx-overflow-menu-item>Option 2</bx-overflow-menu-item>
+          </bx-overflow-menu-body>
+        </bx-breadcrumb-overflow-menu>
+      </bx-breadcrumb-item>
+      <bx-breadcrumb-item>
+        <bx-breadcrumb-link href="/#" aria-current="page">Breadcrumb 3</bx-breadcrumb-link>
+      </bx-breadcrumb-item>
+    </bx-breadcrumb>
+  `,
 });
--- src/components/breadcrumb/breadcrumb-story-react.tsx
+++ src/components/breadcrumb/breadcrumb-story-react.tsx
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2019, 2020
+ * Copyright IBM Corp. 2019, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -16,6 +16,12 @@ import BXBreadcrumb from 'carbon-web-components/es/components-react/breadcrumb/b
 import BXBreadcrumbItem from 'carbon-web-components/es/components-react/breadcrumb/breadcrumb-item';
 // @ts-ignore
 import BXBreadcrumbLink from 'carbon-web-components/es/components-react/breadcrumb/breadcrumb-link';
+// @ts-ignore
+import BXBreadcrumbOverflowMenu from 'carbon-web-components/es/components-react/breadcrumb/breadcrumb-overflow-menu';
+// @ts-ignore
+import BXOverflowMenuBody from 'carbon-web-components/es/components-react/overflow-menu/overflow-menu-body';
+// @ts-ignore
+import BXOverflowMenuItem from 'carbon-web-components/es/components-react/overflow-menu/overflow-menu-item';
 import { Default as baseDefault } from './breadcrumb-story';
 
 export { default } from './breadcrumb-story';
@@ -27,6 +33,14 @@ export const Default = () => (
     </BXBreadcrumbItem>
     <BXBreadcrumbItem>
       <BXBreadcrumbLink href="/#">Breadcrumb 2</BXBreadcrumbLink>
+    </BXBreadcrumbItem>
+    <BXBreadcrumbItem>
+      <BXBreadcrumbOverflowMenu>
+        <BXOverflowMenuBody>
+          <BXOverflowMenuItem>Option 1</BXOverflowMenuItem>
+          <BXOverflowMenuItem>Option 2</BXOverflowMenuItem>
+        </BXOverflowMenuBody>
+      </BXBreadcrumbOverflowMenu>
     </BXBreadcrumbItem>
     <BXBreadcrumbItem>
       <BXBreadcrumbLink href="/#" aria-current="page">
