ce45e4553ce4fc58d494796d83aa5079622cccaa
emyarod <afw5059@gmail.com>
2022-03-10 17:08:35 -0600

    feat(overflow-menu-body): implement  keyboard input circular navigation

--- src/components/overflow-menu/defs.ts
+++ src/components/overflow-menu/defs.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -28,3 +28,8 @@ export enum OVERFLOW_MENU_SIZE {
    */
   EXTRA_LARGE = 'xl',
 }
+
+export const NAVIGATION_DIRECTION = {
+  ArrowDown: 1,
+  ArrowUp: -1,
+};
