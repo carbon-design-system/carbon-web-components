1dc8a4e3b2f76206a4570675e2d64ca81afed33c
Anna Wen <54281166+annawen1@users.noreply.github.com>
2022-06-13 13:01:26 -0400

    chore(typescript): adjust config to remove errors

--- src/globals/mixins/validity.ts
+++ src/globals/mixins/validity.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright IBM Corp. 2020
+ * Copyright IBM Corp. 2020, 2022
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -97,6 +97,7 @@ const ValidityMixin = <T extends Constructor<HTMLElement>>(Base: T) => {
           )
         ) {
           this.invalid = true;
+          // @ts-ignore
           this.validityMessage = this._getValidityMessage(status);
         }
         return false;
